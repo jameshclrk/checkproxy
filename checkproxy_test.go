cafdee019d6f2f435f722419e39576b000245e0f
James Clark <james@jamesclark.dev>
2021-09-13 21:06:26 +0100

    Formatting changes

--- checkproxy_test.go
+++ checkproxy_test.go
@@ -1,16 +1,16 @@
 package checkproxy
 
 import (
-    "bytes"
-    "net/http"
-    "net/http/httptest"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 var ok []byte = []byte("Excellent!!")
 
 func okResponse(w http.ResponseWriter, r *http.Request) {
-    w.Write(ok)
+	w.Write(ok)
 }
 
 func TestCheckIPInNetworkList(t *testing.T) {
@@ -39,102 +39,115 @@ func TestCheckIPInNetworkList(t *testing.T) {
 }
 
 func TestCheckProxyDisabledWithForwarded(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	req.Header.Add("X-Forwarded-For", "100.100.100.100")
 	rec := httptest.NewRecorder()
 
-
-    CheckProxy(false, []string{""})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+	CheckProxy(false, []string{""})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
 
 	if rec.Code != http.StatusBadRequest {
 		t.Fatal("Response Code should be 400")
 	}
-    body := rec.Body.Bytes()
-    if bytes.Compare(body, []byte("Detected proxy: but application is not configured to use one")) != 0 {
-        t.Fatalf("Unexpected output (got %v)", string(body))
-    }
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, []byte("Detected proxy: but application is not configured to use one")) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
 }
 
 func TestCheckProxyDisabledWithoutForwarded(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	rec := httptest.NewRecorder()
 
-
-    CheckProxy(false, []string{""})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+	CheckProxy(false, []string{""})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
 
 	if rec.Code != http.StatusOK {
 		t.Fatal("Response Code should be 200")
 	}
-    body := rec.Body.Bytes()
-    if bytes.Compare(body, ok) != 0 {
-        t.Fatalf("Unexpected output (got %v)", string(body))
-    }
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, ok) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
 }
 
 func TestCheckProxyEnabledWithoutForwarded(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	rec := httptest.NewRecorder()
 
-
-    CheckProxy(true, []string{""})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+	CheckProxy(true, []string{""})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
 
 	if rec.Code != http.StatusBadRequest {
 		t.Fatal("Response Code should be 400")
 	}
-    body := rec.Body.Bytes()
-    if bytes.Compare(body, []byte("Expected a proxy: X-Forwarded-For empty")) != 0 {
-        t.Fatalf("Unexpected output (got %v)", string(body))
-    }
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, []byte("Expected a proxy: X-Forwarded-For empty")) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
 }
 
-
 func TestCheckProxyEnabledWithForwardedTrustedProxy(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	req.Header.Add("X-Forwarded-For", "100.100.100.100")
-    req.RemoteAddr = "10.0.0.1:10101"
+	req.RemoteAddr = "10.0.0.1:10101"
 	rec := httptest.NewRecorder()
 
-    CheckProxy(true, []string{"10.0.0.1"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+	CheckProxy(true, []string{"10.0.0.1"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
 
 	if rec.Code != http.StatusOK {
 		t.Fatal("Response Code should be 200")
 	}
-    body := rec.Body.Bytes()
-    if bytes.Compare(body, ok) != 0 {
-        t.Fatalf("Unexpected output (got %v)", string(body))
-    }
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, ok) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
 }
 
 func TestCheckProxyEnabledWithForwardedUntrustedProxy(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	req.Header.Add("X-Forwarded-For", "100.100.100.100")
-    req.RemoteAddr = "10.0.0.1:10101"
+	req.RemoteAddr = "10.0.0.1:10101"
 	rec := httptest.NewRecorder()
 
-    CheckProxy(true, []string{"10.0.0.2"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+	CheckProxy(true, []string{"10.0.0.2"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
 
 	if rec.Code != http.StatusBadRequest {
 		t.Fatal("Response Code should be 400")
 	}
-    body := rec.Body.Bytes()
-    if bytes.Compare(body, []byte("Untrusted proxy")) != 0 {
-        t.Fatalf("Unexpected output (got %v)", string(body))
-    }
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, []byte("Untrusted proxy")) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
 }
 
 func TestCheckProxyEnabledWithForwardedUnexpectedRemoteAddrFormat(t *testing.T) {
-    req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	req.Header.Add("X-Forwarded-For", "100.100.100.100")
-    req.RemoteAddr = "10.0.0.1"
+	req.RemoteAddr = "asdasjd"
 	rec := httptest.NewRecorder()
 
-    CheckProxy(true, []string{"10.0.0.2"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+	CheckProxy(true, []string{"10.0.0.2"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
 
-	if rec.Code != http.StatusInternalServerError {
-		t.Fatal("Response Code should be 500")
+	if rec.Code != http.StatusBadRequest {
+		t.Fatal("Response Code should be 400")
 	}
-    body := rec.Body.Bytes()
-    if bytes.Compare(body, []byte("")) != 0 {
-        t.Fatalf("Unexpected output (got %v)", string(body))
-    }
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, []byte("Untrusted proxy")) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
+}
+
+func TestCheckProxyEnabledWithForwardedIPRemoteAddrFormat(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Forwarded-For", "100.100.100.100")
+	req.RemoteAddr = "10.0.0.1"
+	rec := httptest.NewRecorder()
+
+	CheckProxy(true, []string{"10.0.0.2"})(http.HandlerFunc(okResponse)).ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusBadRequest {
+		t.Fatal("Response Code should be 400")
+	}
+	body := rec.Body.Bytes()
+	if bytes.Compare(body, []byte("Untrusted proxy")) != 0 {
+		t.Fatalf("Unexpected output (got %v)", string(body))
+	}
 }
